bfd2e32f391b54f294ae232a2e1d2edc93980855
Kun Jeong <kunjeong99@naver.com>
2020-04-07 23:28:16 +0900

    setup for vimrc and zshrc

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#backup and copy .vimrc
+if [[ -e ~/.vimrc ]] && [[ `diff $(pwd)/.vimrc ~/.vimrc` ]]; then
+  mv ~/.vimrc ~/.vimrc_backup
+  cp $(pwd)/.vimrc ~/.vimrc
+fi
+if [[ ! -e ~/.vimrc ]]; then
+  cp $(pwd)/.vimrc ~/.vimrc
+fi
+
+#backup and copy .zshrc
+if [[ -e ~/.zshrc ]] && [[ `diff $(pwd)/.zshrc ~/.zshrc` ]]; then
+  mv ~/.zshrc ~/.zshrc_backup
+  cp $(pwd)/.zshrc ~/.zshrc
+fi
+if [[ ! -e ~/.vimrc ]]; then
+  cp $(pwd)/.zshrc ~/.zshrc
+fi
