eb7f44da06aed0c0a3f2453d6b365f0ba3f18ec0
Kun Jeong <kunjeong99@naver.com>
2020-04-07 23:38:56 +0900

    added oh-my-zsh, plugins and themes

--- setup.sh
+++ setup.sh
@@ -17,3 +17,17 @@ fi
 if [[ ! -e ~/.zshrc ]]; then
   cp $(pwd)/.zshrc ~/.zshrc
 fi
+
+#install oh-my-zsh
+if [[ ! -d $HOME/.oh-my-zsh ]]; then
+  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+fi
+if [[ ! -d $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting ]]; then
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+fi
+if [[ ! -d $HOME/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]]; then
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+fi
+if [[ ! -d $HOME/.oh-my-zsh/custom/themes/powerlevel10k ]]; then
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+fi
